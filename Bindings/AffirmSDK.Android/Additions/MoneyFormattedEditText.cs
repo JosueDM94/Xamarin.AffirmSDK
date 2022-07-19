b167e18ff32d54188d306c99c6e01b9db97f2068
Josue Diaz <josue.diaz@distillery.com>
2022-07-19 01:00:25 -0500

    Updated AffirmSDK Android to 2.0.20

---
+++ Bindings/AffirmSDK.Android/Additions/MoneyFormattedEditText.cs
@@ -0,0 +1,19 @@
+﻿using Android.Runtime;
+
+namespace AffirmSDK.Widget
+{
+    partial class MoneyFormattedEditText
+    {
+        // Error CS0534: 'MoneyFormattedEditText' does not implement inherited abstract member 'FormattedEditText.RawTextParser.get'
+        protected override AffirmSDK.ITextParser RawTextParser
+        {
+            get
+            {
+                try {
+                    return global::Java.Lang.Object.GetObject<global::AffirmSDK.ITextParser>(JNIEnv.CallObjectMethod(Handle, JNIEnv.GetMethodID(JNIEnv.GetObjectClass(Handle), "getTextParser", "()Lcom/affirm/android/TextParser;")), JniHandleOwnership.TransferLocalRef);
+                } finally {
+                }
+            }
+        }
+    }
+}
